666141ffd73e90a5a7216a8ec6ffc659f3f25ab7
leecoda <lilingbnu@gmail.com>
2021-09-16 10:23:34 -0230

    hello world and int type

---
+++ helloWorld/src/hello.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        System.out.println("Hello, Ling");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        System.out.println(mySecondNumber);
+        int myThirdNumber = myFirstNumber * 2;
+        System.out.println(myThirdNumber);
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
